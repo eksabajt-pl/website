a3ace9a86c4308666fdf67279f9c90a6f9767e34
Robert Pławski <robertplawski8@gmail.com>
2025-03-31 15:12:47 +0200

    Added lottie programmer animation, not final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,57 @@
+"use client"
 import CtaButton from "@/components/buttons/CtaButton";
 import Header from "@/components/header/Header";
 import TextGradient from "@/components/text/TextGradient";
 import { Fade } from "react-awesome-reveal";
 import Wave from "react-wavify";
 import Contact from "@/components/contact/Contact";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="font-sans gap-8 p-8 flex flex-col gap-4 min-h-[80vh] items-center justify-center">
-        <div className="flex flex-row gap-4 text-5xl">
-          <Fade triggerOnce cascade damping={0.05}>
-            Czym jest
-          </Fade>
-          <TextGradient>
-            {" "}
-            <Fade
-              triggerOnce
-              delay={500}
-              className="font-bold"
-              cascade
-              damping={0.05}
-            >
-              eksabajt?
+      <main className="font-sans gap-8 p-8 flex flex-row gap-4 min-h-[80vh] items-center justify-center">
+        <div className="flex-col items-center flex gap-8">
+          <DotLottieReact autoplay loop src="https://lottie.host/b4014534-9c95-4c41-b369-127a2dc69f1d/g50Lzd41Ky.json"/>
+          <div className="flex flex-row gap-4 text-5xl">
+            <Fade triggerOnce cascade damping={0.05}>
+              Czym jest
             </Fade>
-          </TextGradient>
-        </div>
-        <ul className="gap-2 text-md max-w-lg flex-col flex list-disc pl-10">
-          <Fade triggerOnce cascade damping={1} delay={1500}>
-            <li>to kolejna jednostka informatyczna po petabajcie</li>
-            <li>to jednostka wynosząca 10^18 bajta</li>
-            <li>
-              to ilość danych globalnie wysyłanych przez internet co godzinę
-            </li>
-            <li>
-              co najważniejsze, to{" "}
-              <span className="font-bold">grupa programistów</span>, z Opola,
-              która{" "}
-              <span className="font-bold">
-                wyceni, zaprojektuje i zaprogramuje twoją stronę
-              </span>
-            </li>
+            <TextGradient>
+              {" "}
+              <Fade
+                triggerOnce
+                delay={500}
+                className="font-bold"
+                cascade
+                damping={0.05}
+              >
+                eksabajt?
+              </Fade>
+            </TextGradient>
+          </div>
+          <ul className="gap-2 text-md max-w-lg flex-col flex list-disc pl-10">
+            <Fade triggerOnce cascade damping={1} delay={1500}>
+              <li>to kolejna jednostka informatyczna po petabajcie</li>
+              <li>to jednostka wynosząca 10^18 bajta</li>
+              <li>
+                to ilość danych globalnie wysyłanych przez internet co godzinę
+              </li>
+              <li>
+                co najważniejsze, to{" "}
+                <span className="font-bold">grupa programistów</span>, z Opola,
+                która{" "}
+                <span className="font-bold">
+                  wyceni, zaprojektuje i zaprogramuje twoją stronę
+                </span>
+              </li>
+            </Fade>
+          </ul>
+          <Fade delay={5500}>
+            <CtaButton>Skontaktuj się z nami!</CtaButton>
           </Fade>
-        </ul>
-        <Fade delay={5500}>
-          <CtaButton>Skontaktuj się z nami!</CtaButton>
-        </Fade>
+        </div>
       </main>
       <div className="flex flex-col">
       <Wave fill="url(#gradient)">
