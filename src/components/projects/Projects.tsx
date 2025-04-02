0b8b41a2343d1e6ec360e0b1a8cb22f1d3c7aa85
W0jtases <wjtases@gmail.com>
2025-04-02 13:47:25 +0200

    hotfix/Carousele_components

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { SquareArrowOutUpRight } from "lucide-react";
 import { SiSocketdotio } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
-
+import CarouselSlot from "../carousel/carouselItem/Carousel";
 import {
 	Carousel,
 	CarouselContent,
@@ -26,117 +26,30 @@ export default function Home() {
 							<h1 className="text-4xl font-bold text-center">Nasze projekty</h1>
 							<Carousel>
 								<CarouselContent>
-									<CarouselItem className="1/2 flex items-center justify-center">
-										<div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
-											<div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
-												<img
-													src="no_bg.png"
-													className="h-full w-full rounded-3xl"
-												/>
-												<div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
-													<img
-														src="no_logo.png"
-														className="h-full w-full rounded-sm"
-													/>
-												</div>
-											</div>
-											<div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
-												<a
-													className="flex flex-row items-center gap-2"
-													href="https://github.com/robertplawski/a-card-game"
-												>
-													<h1 className="text-2xl">NO! Card game </h1>
-													<SquareArrowOutUpRight size={20} />
-												</a>
-												<p className="text-lg">
-													Play a card game with your friends, game based on
-													popular UNO card game
-												</p>
-											</div>
-											<div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<SiSocketdotio /> Socket io
-												</p>
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<FaReact /> React
-												</p>
-											</div>
-										</div>
-									</CarouselItem>
-									<CarouselItem className="1/2 flex items-center justify-center">
-										<div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
-											<div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
-												<img
-													src="no_bg.png"
-													className="h-full w-full rounded-3xl"
-												/>
-												<div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
-													<img
-														src="no_logo.png"
-														className="h-full w-full rounded-sm"
-													/>
-												</div>
-											</div>
-											<div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
-												<a
-													className="flex flex-row items-center gap-2"
-													href="https://github.com/robertplawski/a-card-game"
-												>
-													<h1 className="text-2xl">NO! Card game </h1>
-													<SquareArrowOutUpRight size={20} />
-												</a>
-												<p className="text-lg">
-													Play a card game with your friends, game based on
-													popular UNO card game
-												</p>
-											</div>
-											<div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<SiSocketdotio /> Socket io
-												</p>
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<FaReact /> React
-												</p>
-											</div>
-										</div>
-									</CarouselItem>
-									<CarouselItem className="1/2 flex items-center justify-center">
-										<div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
-											<div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
-												<img
-													src="no_bg.png"
-													className="h-full w-full rounded-3xl"
-												/>
-												<div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
-													<img
-														src="no_logo.png"
-														className="h-full w-full rounded-sm"
-													/>
-												</div>
-											</div>
-											<div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
-												<a
-													className="flex flex-row items-center gap-2"
-													href="https://github.com/robertplawski/a-card-game"
-												>
-													<h1 className="text-2xl">NO! Card game </h1>
-													<SquareArrowOutUpRight size={20} />
-												</a>
-												<p className="text-lg">
-													Play a card game with your friends, game based on
-													popular UNO card game
-												</p>
-											</div>
-											<div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<SiSocketdotio /> Socket io
-												</p>
-												<p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4">
-													<FaReact /> React
-												</p>
-											</div>
-										</div>
-									</CarouselItem>
+									<CarouselSlot
+										name={"NO! Card game"}
+										desc={"Play a card game with your friends, game based on popular UNO card game"}
+										banner={"no_bg.png"}
+										logo={"no_logo.png"}
+									/>
+									<CarouselSlot
+										name={"NO! Card game"}
+										desc={"Play a card game with your friends, game based on popular UNO card game"}
+										banner={"no_bg.png"}
+										logo={"no_logo.png"}
+									/>
+									<CarouselSlot
+										name={"NO! Card game"}
+										desc={"Play a card game with your friends, game based on popular UNO card game"}
+										banner={"no_bg.png"}
+										logo={"no_logo.png"}
+									/>
+									<CarouselSlot
+										name={"NO! Card game"}
+										desc={"Play a card game with your friends, game based on popular UNO card game"}
+										banner={"no_bg.png"}
+										logo={"no_logo.png"}
+									/>
 								</CarouselContent>
 							</Carousel>
 						</div>
