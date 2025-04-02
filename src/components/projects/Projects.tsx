fce1e8a20ed809be764df3a19ad4184b425f6cd9
W0jtases <wjtases@gmail.com>
2025-04-02 12:38:24 +0200

    update/projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,6 +3,11 @@ import Contact from "@/components/contact/Contact";
 import BrandWave from "@/components/pearls/BrandWave";
 import HeroSection from "@/components/sections/HeroSection";
 
+import React from 'react';
+import { SquareArrowOutUpRight } from 'lucide-react';
+import { SiSocketdotio } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+
 import {
     Carousel,
     CarouselContent,
@@ -26,50 +31,59 @@ export default function Home() {
                         <Carousel>
                             <CarouselContent>
                             <CarouselItem className="1/2 flex items-center justify-center">
-                                    <div className="w-70 h-100 border-3 border-green-600 rounded-3xl">
-                                    
-                                        <div className="w-full h-1/5 rounded-t-3xl flex items-center pl-4 gap-4">
-                                        <img src="robert.jpeg" className="h-16 rounded-full" />
-                                        <h1 className="text-xl font-bold">Robert Pławski</h1>
+                                    <div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
+                                    <div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
+                                        <img src="no_bg.png" className="h-full w-full rounded-3xl" />
+                                        <div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
+                                            
+                                            <img src="no_logo.png" className="h-full w-full rounded-sm" />
                                         </div>
-                                    <div className="w-full h-3/5 flex items-center flex-col pl-4 gap-4">
-                                     <h1 className="text-2xl">Title</h1>
-                                     <p className="text-lg">[opis...]</p>
-                                    </div>
-                                        <div className="h-1/5 w-full rounded-b-3xl flex items-center justify-end pr-4">
-                                            <button className="bg-green-500 py-4 px-8 rounded-3xl">Zobacz</button>
                                         </div>
+                                    <div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
+                                     <a className="flex flex-row items-center gap-2" href="https://github.com/robertplawski/a-card-game"><h1 className="text-2xl">NO! Card game </h1><SquareArrowOutUpRight size={20} /></a> 
+                                     <p className="text-lg">Play a card game with your friends, game based on popular UNO card game</p>
                                     </div>
-                                </CarouselItem>
-                                <CarouselItem className="1/2 flex items-center justify-center">
-                                    <div className="w-70 h-100 border-3 border-green-600 rounded-3xl">
-                                    
-                                        <div className="w-full h-1/5 rounded-t-3xl flex items-center pl-4 gap-4">
-                                        <img src="robert.jpeg" className="h-16 rounded-full" />
-                                        <h1 className="text-xl font-bold">Robert Pławski</h1>
-                                        </div>
-                                    <div className="w-full h-3/5 flex items-center flex-col pl-4 gap-4">
-                                     <h1 className="text-2xl">Title</h1>
-                                     <p className="text-lg">[opis...]</p>
-                                    </div>
-                                        <div className="h-1/5 w-full rounded-b-3xl flex items-center justify-end pr-4">
-                                            <button className="bg-green-500 py-4 px-8 rounded-3xl">Zobacz</button>
+                                        <div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><SiSocketdotio /> Socket io</p> 
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><FaReact /> React</p> 
                                         </div>
                                     </div>
                                 </CarouselItem>
                                 <CarouselItem className="1/2 flex items-center justify-center">
-                                    <div className="w-70 h-100 border-3 border-green-600 rounded-3xl">
-                                    
-                                        <div className="w-full h-1/5 rounded-t-3xl flex items-center pl-4 gap-4">
-                                        <img src="robert.jpeg" className="h-16 rounded-full" />
-                                        <h1 className="text-xl font-bold">Robert Pławski</h1>
+                                    <div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
+                                    <div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
+                                        <img src="no_bg.png" className="h-full w-full rounded-3xl" />
+                                        <div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
+                                            
+                                            <img src="no_logo.png" className="h-full w-full rounded-sm" />
                                         </div>
-                                    <div className="w-full h-3/5 flex items-center flex-col pl-4 gap-4">
-                                     <h1 className="text-2xl">Title</h1>
-                                     <p className="text-lg">[opis...]</p>
+                                        </div>
+                                    <div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
+                                     <a className="flex flex-row items-center gap-2" href="https://github.com/robertplawski/a-card-game"><h1 className="text-2xl">NO! Card game </h1><SquareArrowOutUpRight size={20} /></a> 
+                                     <p className="text-lg">Play a card game with your friends, game based on popular UNO card game</p>
                                     </div>
-                                        <div className="h-1/5 w-full rounded-b-3xl flex items-center justify-end pr-4">
-                                            <button className="bg-green-500 py-4 px-8 rounded-3xl">Zobacz</button>
+                                        <div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><SiSocketdotio /> Socket io</p> 
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><FaReact /> React</p> 
+                                        </div>
+                                    </div>
+                                </CarouselItem>
+                                <CarouselItem className="1/2 flex items-center justify-center">
+                                    <div className="w-110 h-130 border-3 border-green-600 rounded-3xl">
+                                    <div className="w-full h-3/6 rounded-t-2xl flex items-center bg-[#32a834] p-3 gap-4 relative">
+                                        <img src="no_bg.png" className="h-full w-full rounded-3xl" />
+                                        <div className="h-22 w-22 z-10 bg-white absolute border-8 border-[#32a834] rounded-xl top-50 left-80">
+                                            
+                                            <img src="no_logo.png" className="h-full w-full rounded-sm" />
+                                        </div>
+                                        </div>
+                                    <div className="w-full h-2/6 flex pl-4 flex-col bg-[#32a834] gap-4">
+                                     <a className="flex flex-row items-center gap-2" href="https://github.com/robertplawski/a-card-game"><h1 className="text-2xl">NO! Card game </h1><SquareArrowOutUpRight size={20} /></a> 
+                                     <p className="text-lg">Play a card game with your friends, game based on popular UNO card game</p>
+                                    </div>
+                                        <div className="h-1/6 w-full text-xl font-bold rounded-b-3xl bg-[#32a834] rounded-b-xl flex items-center gap-3 justify-start pl-4">
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><SiSocketdotio /> Socket io</p> 
+                                            <p className="px-8 py-3 rounded-2xl bg-[#297d2b] flex items-center justify-center gap-4"><FaReact /> React</p> 
                                         </div>
                                     </div>
                                 </CarouselItem>
