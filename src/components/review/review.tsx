c98007c1c7acf2a5fcc910191d61f50f62523ead
unknown <maciekp371@gmail.com>
2025-04-02 17:11:17 +0200

    scale improvement

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -6,7 +6,7 @@ import Fade from "../pearls/Fade";
 import TextGradient from "../text/TextGradient";
 const Review = () => {
 const arrays = (stars:number) => {
-    return Array(stars).fill(<FontAwesomeIcon className="text-xl max-lg:text-lg max-md:text-2xl text-green-500" icon={faStar} />)
+    return Array(stars).fill(<FontAwesomeIcon className="text-lg ml-[10px] text-green-500" icon={faStar} />)
     .map((star,index) => <span key={index}>{star}</span>)
 }
 
@@ -29,7 +29,7 @@ const arrays = (stars:number) => {
     <div className="min-h-[250px] max-w flex flex-row flex-wrap justify-center items-start">
 {jsonFile?.map((value,index)=> {
     return(
-        <div className="h-[100%] p-4 my-3 rounded-2xl w-[33%] max-md:w-[100%] max-md:min-h[150px]  max-lg:p-1" key={index}><div className='flex flex-row justify-between'><div className="flex justify-start items-center w-[100%] text-3xl my-3 text-stone-850 font-black max-md:text-2xl max-md:m-0">{value.name}</div><div className=' flex justify-center items-center my-3  text-xl font-bold text-center'>{arrays(value.stars)}</div></div><div className="p-1 text-xl leading-10 max-md:leading-7 text-wrap max-md:text-base">{value.content}</div></div>
+        <div className="h-[100%] p-4 my-3 rounded-2xl w-[33%] max-md:w-[100%] max-md:min-h[150px]  max-lg:p-1" key={index}><div className='flex flex-row justify-start'><div className="flex justify-start items-center  text-3xl my-3 text-stone-850 font-black max-md:text-2xl max-md:m-0">{value.name}</div><div className=' flex justify-center items-center my-3  text-xl font-bold text-center'>{arrays(value.stars)}</div></div><div className="p-1 text-xl leading-10 max-md:leading-7 text-wrap max-md:text-base">{value.content}</div></div>
     );
 })}
     </div>
