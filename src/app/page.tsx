724a813e50cb38cd5fc274bc6fa4859e88eea424
Robert Plawski <dev@robertplawski.pl>
2025-04-03 23:20:42 +0200

    Deleted brand wave from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/header/Header";
 import ReviewsSection from "@/components/sections/ReviewSection";
 import ContactSection from "@/components/sections/ContactSection";
-import BrandWave from "@/components/pearls/BrandWave";
 import HeroSection from "@/components/sections/HeroSection";
 import TeamSection from "@/components/sections/TeamSection";
 import Footer from "@/components/footer/Footer";
@@ -11,7 +10,6 @@ export default function Home() {
     <>
       <Header />
       <HeroSection />
-      <BrandWave />
       <ReviewsSection />
       <TeamSection />
       <ContactSection />
