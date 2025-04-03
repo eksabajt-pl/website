2c2e6efa1c0ba536c6df59e8ae642e822d33f3a2
Robert Pławski <robertplawski8@gmail.com>
2025-04-03 10:13:20 +0200

    Fixed eslint types

--- src/components/team/teamBox/TeamBox.tsx
+++ src/components/team/teamBox/TeamBox.tsx
@@ -2,42 +2,48 @@ import TextGradient from "@/components/text/TextGradient";
 import { Fade } from "react-awesome-reveal";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 interface TeamBoxProps {
-  text: string;
-  desc: string;
-  delay: number;
+	text: string;
+	desc: string;
+	delay: number;
 }
 
 export default function TeamBox({ text, desc, delay }: TeamBoxProps) {
-  return (
-    <>
-      <Fade direction="up" delay={delay} triggerOnce>
-        <div className="w-70 p-4 hover:scale-110 duration-200">
-          <Card>
-            <CardHeader>
-              <CardTitle className="flex justify-center">
-                <TextGradient>
-                  <p className="text-2xl">{text}</p>
-                </TextGradient>
-              </CardTitle>
-              <CardDescription className="flex justify-center">
-                <p className="text-md pb-2">{desc}</p>
-              </CardDescription>
-              <CardContent className="flex justify-center">
-                {/* tu bendom zdjecia :D */}
-                <img src="/profile/image.png" className="rounded-full w-40" />
-              </CardContent>
-            </CardHeader>
-          </Card>
-        </div>
-      </Fade>
-    </>
-  );
+	return (
+		<>
+			<Fade direction="up" delay={delay} triggerOnce>
+				<div className="w-70 p-4 hover:scale-110 duration-200">
+					<Card>
+						<CardHeader>
+							<CardTitle className="flex justify-center">
+								<TextGradient>
+									<p className="text-2xl">{text}</p>
+								</TextGradient>
+							</CardTitle>
+							<CardDescription className="flex justify-center">
+								<p className="text-md pb-2">{desc}</p>
+							</CardDescription>
+							<CardContent className="flex justify-center">
+								{/* tu bendom zdjecia :D */}
+								<Image
+									width={160}
+									height={160}
+									alt={text + "'s profile picture"}
+									src="/profile/image.png"
+									className="rounded-full w-40"
+								/>
+							</CardContent>
+						</CardHeader>
+					</Card>
+				</div>
+			</Fade>
+		</>
+	);
 }
