6113edcd530d39c832d3122c9a245eb30b627672
Robert Plawski <dev@robertplawski.pl>
2025-04-07 01:12:53 +0200

    Updated dashboard suspense

--- src/components/dashboards/Dashboard.tsx
+++ src/components/dashboards/Dashboard.tsx
@@ -1,11 +1,9 @@
-"use server";
-
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { PropsWithChildren, Suspense } from "react";
 import Sidebar from "../sidebars/Sidebar";
 import { DashboardSidebar } from "../sidebars/DashboardSidebar";
 
-export default async function Dashboard({ children }: PropsWithChildren) {
+export default function Dashboard({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-row flex-1 w-full">
       <Suspense fallback={<DashboardSidebar pages={[]} />}>
