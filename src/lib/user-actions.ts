7d3e60e239d0782e3932ddab44cdc1d4d4cbe26e
Robert Plawski <dev@robertplawski.pl>
2025-04-08 17:47:00 +0200

    Fixed email config

--- src/lib/user-actions.ts
+++ src/lib/user-actions.ts
@@ -12,14 +12,14 @@ async function wait(ms: number): Promise<void> {
 export default async function contactForm(formData: ContactFormData) {
   const { tier, username, email, message } = formData;
   await resend.emails.send({
-    from: "Kontakt <kontakt@eksabajt.pl>",
+    from: "Kontakt <noreply@eksabajt.pl>",
     to: [email],
     subject: "Dziękujemy za kontakt z Eksabajt.pl",
     react: "Dziękujemy za kontakt", //EmailTemplate({ firstName: "John" }),
   });
 
   const { data, error } = await resend.emails.send({
-    from: `${username} <kontakt@eksabajt.pl>`,
+    from: `${username} (${email}) <noreply@eksabajt.pl>`,
     to: ["kontakt@eksabajt.pl"],
     subject: `Formularz kontaktowy, tier ${tier}`,
     react: message, //EmailTemplate({ firstName: "John" }),
