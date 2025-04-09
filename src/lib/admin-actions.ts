255aceb465ebc1d616b594d43af2b9039b587b84
Robert Plawski <dev@robertplawski.pl>
2025-04-09 23:38:55 +0200

    Added revalidate path on admin actions

--- src/lib/admin-actions.ts
+++ src/lib/admin-actions.ts
@@ -2,17 +2,24 @@
 
 import { changeReviewStatus } from "@/db/review/changeReviewStatus";
 import { deleteReview } from "@/db/review/deleteReview";
+import { revalidatePath } from "next/cache";
 
 export async function rejectReviewWithId(reviewId: number) {
   await changeReviewStatus(reviewId, "rejected");
+  revalidatePath("/dashboard/reviews", "page");
+  revalidatePath("/", "page");
 }
 
 export async function verifyReviewWithId(reviewId: number) {
   await changeReviewStatus(reviewId, "verified");
+  revalidatePath("/dashboard/reviews", "page");
+  revalidatePath("/", "page");
 }
 
 export async function deleteReviewWithId(reviewId: number) {
   await deleteReview(reviewId);
+  revalidatePath("/dashboard/reviews", "page");
+  revalidatePath("/", "page");
 }
 
 // TODO: implement this function
