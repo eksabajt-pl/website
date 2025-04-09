d004fa902683630db17a381bc9c4581207c1944b
Robert Pławski <robertplawski8@gmail.com>
2025-04-09 15:21:33 +0200

    Fixes per David's request

--- src/components/cards/PricingCard.tsx
+++ src/components/cards/PricingCard.tsx
@@ -7,70 +7,68 @@ import { useCallback } from "react";
 import { redirect } from "next/navigation";
 import { useFormContext } from "react-hook-form";
 export interface PricingCardType {
-  tier: "cheap" | "landing" | "startup" | "professional";
-  title: string;
-  price: string;
-  icon: React.ElementType;
-  unique: boolean;
-  subtitle: string;
-  features: string[];
+	tier: "cheap" | "landing" | "startup" | "professional";
+	title: string;
+	price: string;
+	icon: React.ElementType;
+	unique: boolean;
+	subtitle: string;
+	features: string[];
 }
 
 export const PricingCard = ({
-  tier,
-  title,
-  price,
-  icon: Icon,
-  unique,
-  subtitle,
-  features,
+	tier,
+	title,
+	price,
+	icon: Icon,
+	unique,
+	subtitle,
+	features,
 }: PricingCardType) => {
-  const form = useFormContext();
+	const form = useFormContext();
 
-  const orderButtonHandler = useCallback(() => {
-    form.setValue("tier", tier);
-    redirect(`#contact`);
-  }, [tier, form]);
+	const orderButtonHandler = useCallback(() => {
+		form.setValue("tier", tier);
+		redirect(`#contact`);
+	}, [tier, form]);
 
-  return (
-    <CustomCard
-      className={twMerge(
-        `relative flex justify-between shadow-none border-neutral-700`,
-        unique && "border-green-400"
-      )}
-    >
-      <Icon className="absolute inset-0 text-award " size="100%" />
-      {unique ? (
-        <Badge className="-top-4 absolute left-[50%] -translate-x-[50%] ">
-          Najczęściej wybierany
-        </Badge>
-      ) : null}
+	return (
+		<CustomCard
+			className={twMerge(
+				`relative flex justify-between shadow-none border-neutral-700`,
+				unique && "border-green-400"
+			)}
+		>
+			<Icon className="absolute inset-0 text-award " size="100%" />
+			{unique ? (
+				<Badge className="-top-4 absolute left-[50%] -translate-x-[50%] ">
+					Najczęściej wybierany
+				</Badge>
+			) : null}
 
-      {/* Górna sekcja */}
-      <section className="gap-2 z-2 flex flex-col justify-around text-md sm:text-lg ">
-        <h3 className="text-center font-bold   ">{title}</h3>
-        <h4 className="text-center text-green-400 font-bold tracking-widest text-sm sm:text-base">
-          {price}
-        </h4>
-        <p className="text-li text-center text-sm sm:text-base ">{subtitle}</p>
-      </section>
+			{/* Górna sekcja */}
+			<section className="gap-2 z-2 flex flex-col justify-around text-xl sm:text-2xl ">
+				<h3 className="text-center font-bold   ">{title}</h3>
+				<h4 className="text-center text-green-400 font-bold tracking-widest text-lg sm:text-xl">
+					{price}
+				</h4>
+				<p className="text-li text-center text-base sm:text-lg ">{subtitle}</p>
+			</section>
 
-      <div className="mx-5 my-3 relative flex items-center justify-center max-w"></div>
-      <hr className="z-2 mx-5" />
-      {/* Dolna sekcja */}
-      <section className="z-2 px-4 py-6 flex flex-col text-sm sm:text-base">
-        <ul className="space-y-2">
-          {features.map((feature, index) => (
-            <Li key={index}>{feature}</Li>
-          ))}
-        </ul>
-      </section>
-      <button
-        onClick={orderButtonHandler}
-        className="z-1 text-sm flex-row font-bold p-1 flex justify-center items-center gap-2 bg-green-600 mx-4 cursor-pointer rounded-lg"
-      >
-        <MailCheckIcon size={20} /> Zamów już dziś
-      </button>
-    </CustomCard>
-  );
+			{/* Dolna sekcja */}
+			<section className="z-2 px-4 py-4 flex flex-col text-sm sm:text-base ">
+				<ul className="space-y-2">
+					{features.map((feature, index) => (
+						<Li key={index}>{feature}</Li>
+					))}
+				</ul>
+			</section>
+			<button
+				onClick={orderButtonHandler}
+				className="z-1 text-sm flex-row font-bold p-1 flex justify-center items-center gap-2 bg-green-600 mx-4 cursor-pointer rounded-lg"
+			>
+				<MailCheckIcon size={20} /> Zamów już dziś
+			</button>
+		</CustomCard>
+	);
 };
