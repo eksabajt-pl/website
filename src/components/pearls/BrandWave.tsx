71b4e98fe60938ac14d77c015233c81a563bf9c0
Robert Pławski <robertplawski8@gmail.com>
2025-04-09 13:38:31 +0200

    Added content to page and added glowing

--- src/components/pearls/BrandWave.tsx
+++ src/components/pearls/BrandWave.tsx
@@ -2,21 +2,25 @@ import Wave from "react-wavify";
 import { twMerge } from "tailwind-merge";
 
 type BrandWaveProps = {
-  flipped?: boolean;
+	flipped?: boolean;
 };
 
 export default function BrandWave({ flipped = false }: BrandWaveProps) {
-  return (
-    <Wave
-      fill="url(#gradient)"
-      className={twMerge("wave", flipped && "rotate-x-180")}
-    >
-      <defs>
-        <linearGradient id="gradient" gradientTransform="rotate(90)">
-          <stop offset="10%" stopColor="oklch(0.627 0.194 149.214)" />
-          <stop offset="90%" stopColor="oklch(0.627 0.194 149.214)" />
-        </linearGradient>
-      </defs>
-    </Wave>
-  );
+	return (
+		<div className="relative m-0 -z-10 -mb-1">
+			<div className="-z-20 absolute bottom-[25%] left-[50%] -translate-x-[50%] rounded-full w-[200%] h-[5rem] dark:bg-green-500/60 blur-[10rem]"></div>
+
+			<Wave
+				fill="url(#gradient)"
+				className={twMerge("wave", flipped && "rotate-x-180")}
+			>
+				<defs>
+					<linearGradient id="gradient" gradientTransform="rotate(90)">
+						<stop offset="10%" stopColor="oklch(0.627 0.194 149.214)" />
+						<stop offset="90%" stopColor="oklch(0.627 0.194 149.214)" />
+					</linearGradient>
+				</defs>
+			</Wave>
+		</div>
+	);
 }
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -3,40 +3,43 @@ import Fade from "../pearls/Fade";
 import TextGradient from "../text/TextGradient";
 
 export default function HeroSection() {
-  return (
-    <section className="font-sans gap-8 p-8 flex flex-col gap-4 min-h-[80vh] items-center justify-center">
-      <div className="flex flex-row gap-4 text-5xl justify-center flex-wrap">
-        <Fade triggerOnce cascade damping={0.05}>
-          Czym jest
-        </Fade>
-        <TextGradient>
-          {" "}
-          <Fade
-            triggerOnce
-            delay={500}
-            className="font-bold"
-            cascade
-            damping={0.05}
-          >
-            eksabajt?
-          </Fade>
-        </TextGradient>
-      </div>
-      <ul className="gap-2 text-md max-w-lg flex-col flex list-disc pl-10">
-        <li>to kolejna jednostka informatyczna po petabajcie</li>
-        <li>to jednostka wynosząca 10^18 bajta</li>
-        <li>to ilość danych globalnie wysyłanych przez internet co godzinę</li>
-        <li>
-          co najważniejsze, to{" "}
-          <span className="font-bold">grupa programistów</span>, z Opola, która{" "}
-          <span className="font-bold">
-            wyceni, zaprojektuje i zaprogramuje twoją stronę
-          </span>
-        </li>
-      </ul>
-      <Fade delay={5500}>
-        <CtaButton />
-      </Fade>
-    </section>
-  );
+	return (
+		<section className="font-sans relative gap-8 p-8 flex flex-col gap-4 min-h-[80vh] items-center justify-center">
+			<div className="-z-20 w-auto h-[40rem] top-[50%] -translate-y-[50%] aspect-square absolute rounded-full  dark:bg-green-500/10 blur-[10rem]"></div>
+
+			<div className="flex  flex-row gap-4 text-5xl justify-center flex-wrap">
+				<Fade triggerOnce cascade damping={0.05}>
+					Czym jest
+				</Fade>
+				<TextGradient>
+					{" "}
+					<Fade
+						triggerOnce
+						delay={500}
+						className="font-bold"
+						cascade
+						damping={0.05}
+					>
+						eksabajt?
+					</Fade>
+				</TextGradient>
+			</div>
+			<ul className="gap-2 text-md max-w-lg flex-col flex list-disc pl-10 text-muted-foreground">
+				<li>to kolejna jednostka informatyczna po petabajcie</li>
+				<li>to jednostka wynosząca 10^18 bajta</li>
+				<li>to ilość danych globalnie wysyłanych przez internet co godzinę</li>
+				<li className="text-foreground">
+					co najważniejsze, to{" "}
+					<span className="font-bold">firma programistyczna</span>, z Opola,
+					która{" "}
+					<span className="font-bold">
+						wyceni, zaprojektuje i zaprogramuje twoją stronę
+					</span>
+				</li>
+			</ul>
+			<Fade delay={5500}>
+				<CtaButton />
+			</Fade>
+		</section>
+	);
 }
